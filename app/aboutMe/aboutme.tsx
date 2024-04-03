580449a5e541ce3a64cc1189b7475236a0ea7d30
potpot <hugo.pot666@gmail.com>
2024-04-03 20:26:20 +0200

    fix portfolio

--- app/aboutMe/aboutme.tsx
+++ app/aboutMe/aboutme.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
-
+import Image from 'next/image';
 const AboutMeBox = () => {
     return (
         <div className="flex flex-col items-center justify-start pt-16">
             <div className="bg-gray-100 p-6 rounded-md shadow-md max-w-lg w-full text-center">
                 <div className="mx-auto mb-4 w-24 h-24 rounded-full overflow-hidden">
-                    <img
-                        src="/image/pp.png" // Assurez-vous que le chemin est correct
-                        alt="Your Image"
-                        className="w-full h-full object-cover"
-                    />
+                    <Image src="/image/pp.avif" alt="Your Image" className="w-full h-full object-cover" width={100} height={100}/>
                 </div>
                 <div>
                     <h1 className="text-xl text-black font-semibold mb-2">Hugo Potier</h1>
--- app/work/page.tsx
+++ app/work/page.tsx
@@ -26,14 +26,16 @@ const WorkPage = () => {
     ];
 
     return (
-        <div className="max-w-6xl mx-auto py-8">
-            <h1 className="text-3xl font-bold mb-8 text-center">Expériences</h1>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-                {experiences.map((experiences, index) => (
-                    <div key={index} className="bg-white rounded-lg shadow-md">
-                        <ProjectArticle project={experiences}/>
-                    </div>
-                ))}
+        <div className="h-dvh flex items-center justify-center">
+            <div className="max-w-6xl mx-auto py-8">
+                <h1 className="text-3xl font-bold mb-8 text-center">Expériences</h1>
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+                    {experiences.map((experiences, index) => (
+                        <div key={index} className="bg-white rounded-lg shadow-md">
+                            <ProjectArticle project={experiences}/>
+                        </div>
+                    ))}
+                </div>
             </div>
         </div>
     );
