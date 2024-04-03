7b967e02270592dc127573ab0d94128b801d45d8
potpot <hugo.pot666@gmail.com>
2024-04-03 20:33:51 +0200

    fix portfolio

--- app/aboutMe/aboutme.tsx
+++ app/aboutMe/aboutme.tsx
@@ -5,7 +5,7 @@ const AboutMeBox = () => {
         <div className="flex flex-col items-center justify-start pt-16">
             <div className="bg-gray-100 p-6 rounded-md shadow-md max-w-lg w-full text-center">
                 <div className="mx-auto mb-4 w-24 h-24 rounded-full overflow-hidden">
-                    <Image src="/image/pp.avif" alt="Your Image" className="w-full h-full object-cover" width={100} height={100}/>
+                    <Image src="/image/pp.png" alt="Your Image" className="w-full h-full object-cover" width={100} height={100}/>
                 </div>
                 <div>
                     <h1 className="text-xl text-black font-semibold mb-2">Hugo Potier</h1>
